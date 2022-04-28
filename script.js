120c257d583e69fd97afefcf5718d8b00267070d
Andrew <abysmalduck@gmail.com>
2022-04-28 09:17:42 -0700

    - Add event listeners for buttons that play rounds - Add global variables to keep track of scoreboard, as well as     last winner and last computer choice

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ let scoreBoard = document.getElementById('scoreboard');
 let playerScore = document.getElementById('playerScore');
 let computerScore = document.getElementById('computerScore');
 
+// Initialize global variables for last winner and last computer selection
+let lastComputerSelection;
+let lastWinner;
 
+// Add event listeners for buttons to playRound(<button pressed>)
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+
+rock.addEventListener('click', playRound('rock', computerPlay()));
+paper.addEventListener('click', playRound('paper', computerPlay()));
+scissors.addEventListener('click', playRound('scissors', computerPlay()));
+
+// Add div/logic to show what the computer selected and who won each round
+scoreBoard.textContent = 
+// Modify DOM elements in scoreboard div to show current score
+// Add pizzaz
 
 function computerPlay() {
     // Function to generate random numbers within a range, which for this script will be 1-3.
@@ -44,34 +60,52 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock') { 
         if (computerSelection === 'paper') { 
             console.log('You lose! Paper beats Rock');
+            lastWinner = 'Computer';
+            lastComputerSelection = computerSelection;
             return 'computer';
         } else if (computerSelection === 'scissors') {
             console.log('You win! Rock beats Scissors');
+            lastWinner = 'Player';
+            lastComputerSelection = computerSelection;
             return 'player';
         } else if (computerSelection === 'rock') {
             console.log('Tie between Rock and Rock!');
+            lastWinner = 'Tie';
+            lastComputerSelection = computerSelection;
             return 'tie';
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'paper') { 
             console.log('Tie between Paper and Paper!');
+            lastWinner = 'Tie';
+            lastComputerSelection = computerSelection;
             return 'tie';
         } else if (computerSelection === 'scissors') {
             console.log('You lose! Scissors beats Paper');
+            lastWinner = 'Computer';
+            lastComputerSelection = computerSelection;
             return 'computer';
         } else if (computerSelection === 'rock') {
             console.log('You win! Paper beats Rock');
+            lastWinner = 'Player';
+            lastComputerSelection = computerSelection;
             return 'player';
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') { 
             console.log('You win! Scissors beats Paper');
+            lastWinner = 'Player';
+            lastComputerSelection = computerSelection;
             return 'player';
         } else if (computerSelection === 'scissors') {
             console.log('Tie between Scissors and Scissors!');
+            lastWinner = 'Tie';
+            lastComputerSelection = computerSelection;
             return 'tie';
         } else if (computerSelection === 'rock') {
             console.log('You lose! Rock beats Scissors');
+            lastWinner = 'Computer';
+            lastComputerSelection = computerSelection;
             return 'computer';
         }
     }
@@ -82,7 +116,7 @@ function game() {
     let playerWins = 0;
     let computerWins = 0;
     let ties = 0;
-    // let roundCount = 0; // Removing 5-round maximum
+    let roundCount = 0;
     let playing = true;
     while(playing) {
         playerSelection = window.prompt('Rock, paper, or scissors?', 'rock');
@@ -91,18 +125,18 @@ function game() {
         switch(winner) {
             case 'player':
                 playerWins++;
-                //roundCount++;
+                roundCount++;
                 break;
             case 'computer':
                 computerWins++;
-                //roundCount++;
+                roundCount++;
                 break;
             case 'tie':
                 ties++;
-                //roundCount++;
+                roundCount++;
                 break;
         }
-        // if (roundCount >= 5) {playing = false}
+        if (roundCount >= 5) {declareWinner} // Declare a winner after 5 rounds
     }
     if (playerWins > computerWins) {
         alert(`You win! Final score: Player - ${playerWins} / Computer: ${computerWins} / Ties: ${ties}`);
